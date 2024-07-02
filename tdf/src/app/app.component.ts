399862c27f745c2382210bbe792d48744dc0dccd
itshimanshu010 <himanshu.mau1999@gmail.com>
2024-07-02 12:24:58 +0530

    ct

--- tdf/src/app/app.component.ts
+++ tdf/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'Angular 8';
   topic = ['Angular', 'React', 'Vue'];
 
+  //creating instance of the model
+  userModel = new User('Himanshu','hi@gmail.com',55555542,'','morning',true);
+  //Step 2 is here create a object of the model and pass the value to the model
+  //now my creating userModel it will now easy to bind the user model data to our enrollement form
+  
 
 }
---
+++ tdf/src/app/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    constructor(
+        public name: string,
+        public email: string,
+        public phone: number,
+        public topic: string,
+        public timePreference: string,
+        public subscribe: boolean
+    ) { }
+}
+
+// Step 1 is here first create a model which i created here
